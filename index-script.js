0f0f7752bc2aa42d30b5471408cc4a60699058c1
adorablehobbit <jerickobacar@gmail.com>
2026-02-11 11:57:11 +0800

    feat: skills, experiences, contact me

--- index-script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    // const mainContent = document.getElementById("main-content");
-
-    setTimeout(() => {
-        globalThis.location.href = "main-page.html";
-    }, 2000);
-    
-});
